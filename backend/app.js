39fddff3b2c74a539ac597a5eec4446c0d793ecd
Zaid Malik <mzaidmalik25@gmail.com>
2026-05-27 16:20:10 +0530

    fix: update root response message and change background color in App component

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.get("/",(req, res)=>{
-    res.send("hello gyus fully woking ci cd")
+    res.send("kiya CI CD work kar rha hai ?")
 })
 
 app.use("/api/todo", todoRoutes)
